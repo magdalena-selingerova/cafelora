774ae50b799deb2a8bc5d348aaf13d79f1dc99b5
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-11-01 11:33:18 +0100

    navbar - prolinkování

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+document
+  .getElementsByClassName('btn-home')[0]
+  .addEventListener('click', function () {
+    window.scrollTo(0, document.querySelector('.banner').offsetTop);
+  });
+document
+  .getElementsByClassName('btn-menu')[0]
+  .addEventListener('click', function () {
+    window.scrollTo(0, document.querySelector('.menu').offsetTop);
+  });
+document
+  .getElementsByClassName('btn-galerie')[0]
+  .addEventListener('click', function () {
+    window.scrollTo(0, document.querySelector('.gallery').offsetTop);
+  });
+document
+  .getElementsByClassName('btn-contact')[0]
+  .addEventListener('click', function () {
+    window.scrollTo(0, document.querySelector('.contacts').offsetTop);
+  });
