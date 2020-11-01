c2c3d548f62a4d25955bf7763cc292eaeb677d93
Magdalena H. Selingerová <selingerova.magdalena@gmail.com>
2020-11-01 13:28:13 +0100

    objednání kávy aktivováno

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
-//navbar desktop
+//2. navbar desktop
 document
   .getElementsByClassName('btn-home')[0]
   .addEventListener('click', function () {
@@ -23,16 +23,33 @@ document
   .addEventListener('click', function () {
     window.scrollTo(0, document.querySelector('.contacts').offsetTop);
   });
-// navbar mobile
+// 3. navbar mobile
 const nav = document.querySelector('.nav-closed');
 const navLinks = document.querySelectorAll('.nav a');
 const navLink = document.querySelector('.nav a');
 const navBtn = document.querySelector('#nav-btn');
-
+//posluchač události - přidává třídu nav-closed na celý element nav
 const NavBtnElm = (event) => {
   nav.classList.toggle('nav-closed');
 };
+//po kliknutí na button menu přidá posluchač viz výše
 navBtn.addEventListener('click', NavBtnElm);
+//po kliknutí na odkaz v menu přidá posluchač viz výše
 navLinks.forEach((navLink) => {
   navLink.addEventListener('click', NavBtnElm);
+});
+
+//4. objednávání
+let ordered = false;
+const drink = document.querySelector('.drink__cup');
+const orderBtn = document.querySelector('.order-btn');
+
+orderBtn.addEventListener('click', () => {
+  drink.classList.toggle('drink__cup--selected');
+  if (ordered === false) {
+    orderBtn.textContent = 'zrušit';
+  } else {
+    orderBtn.textContent = 'objednat';
+  }
+  ordered = !ordered;
 });
